52c510cc26acc99f40a5d8ce0d1a3724c91a07a6
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-05-06 22:38:30 +0300

    add Ex3 - Violating ISP - Payment

--- Ex1 Violating LSP - Bird/obj/Debug/net8.0/Ex1 Violating LSP - Bird.AssemblyInfo.cs
+++ Ex1 Violating LSP - Bird/obj/Debug/net8.0/Ex1 Violating LSP - Bird.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex1 Violating LSP - Bird")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59e11d7f53c59ebd9af6042a045064cca590a349")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+833fd518f2b9227bb57a9505e1e65511da00e1bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex1 Violating LSP - Bird")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex1 Violating LSP - Bird")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
